6ea0e235b0ab0a29b5471e5cee1bbe0da6e9e9b7
Anton Bazarov <toxabaz@gmail.com>
2017-06-04 18:06:07 +0300

    init

---
+++ 22DragAndDrop/22DragAndDrop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  22DragAndDrop
+//
+//  Created by Predki on 04.06.17.
+//  Copyright © 2017 BazarJrDeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 22DragAndDrop/22DragAndDrop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  22DragAndDrop
+//
+//  Created by Predki on 04.06.17.
+//  Copyright © 2017 BazarJrDeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
